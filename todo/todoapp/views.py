beaef7d3c13e2cd7dfaf51f306400f7683eb2bfd
yo14 <ifliandry11@gmail.com>
2020-08-04 17:21:26 +0700

    Make register, login, and logout system

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+def home(request):
+    return render(request, 'todoapp/home.html')
+
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todoapp/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todoapp/signupuser.html', {'form': UserCreationForm(), 'error': 'That username has already been taken. Please choose a new username.'})
+        else:
+            return render(request, 'todoapp/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did not match.'})
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todoapp/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todoapp/loginuser.html', {'form': AuthenticationForm(), 'error':'Username and Password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def currenttodos(request):
+    return render(request, 'todoapp/currenttodos.html')
+
+
+
+
