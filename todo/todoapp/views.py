39840a6c33cba2803eff8bb6660325f14b8f015f
yo14 <ifliandry11@gmail.com>
2020-08-09 08:12:35 +0700

    Add edit function to tasks

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
@@ -62,6 +62,21 @@ def createtodo(request):
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, dateCompleted__isnull=True)
     return render(request, 'todoapp/currenttodos.html', {'todos':todos})
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todoapp/viewtodo.html', {'todo':todo, 'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todoapp/viewtodo.html', {'todo': todo, 'error': 'Bad Info'})
+
 
 
 
