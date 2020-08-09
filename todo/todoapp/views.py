8a30d26e611cbfb9b8c6d3a3dd5957f5c6f13ac2
yo14 <ifliandry11@gmail.com>
2020-08-09 09:20:44 +0700

    Add delete function task

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -5,6 +5,7 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 
 def home(request):
     return render(request, 'todoapp/home.html')
@@ -76,6 +77,21 @@ def viewtodo(request, todo_pk):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todoapp/viewtodo.html', {'todo': todo, 'error': 'Bad Info'})
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.dateCompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
 
 
 
